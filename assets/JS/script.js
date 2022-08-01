fa57bc6052aea80c597297eda0a711468a6705e1
Patrick Cohenn <patrickcohenn@gmail.com>
2022-07-31 22:35:19 -0300

    Criando aba projetos

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,3 +6,36 @@ function abrirmenu(){
         BarraMenuAberto.style.width = '0px'
     }
 }
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+//  slidesPerGroup: 3, Colocar 3 grupos//
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    //loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    //Colocado modo dynamico no slider
+    dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView: 2,
+        },
+        950:{
+            slidesPerView: 3,
+        },
+    },
+  });
